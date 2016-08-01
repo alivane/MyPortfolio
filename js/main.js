fb595a231585c4b0c3b4d36c6f89d98adaff2984
alisson gutierrez <alisson gutierrez>
2016-08-01 14:20:47 -0400

    insert of image of background and insert of color to nav

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var contador = 1 ;
 	$('.hola').css('display','none')
 	$('nav').css('display','none')
-	$(".menu_bar").click(function(){
+	$("i").click(function(){
 		$('nav').toggle();
 		$('.hola').toggle();
 		$('.fa-bars').toggle();
