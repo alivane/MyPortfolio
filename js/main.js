a2c265db137b86d1834ea7c0af082c3c9f2ed1e2
AliVane <vaneg643@gmail.com>
2016-08-03 13:16:19 -0400

    insert of class and images to section

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
 		$('.fa-bars').toggle();
 	})
 })
+$(document).ready(function(){
+	myFunction()
+	function cambiarFondo(){
+		$('#slider').css('background-image','url(img/portada1.png)')
+		$('#slider h1').html('<h1>Codigo sin Fronteras</h1>')
+	}
+	function cambiarFondo1(){
+		$('#slider').css('background-image','url(img/cabeza.jpg)')
+		$('#slider h1').html('<h1>Soy Front-End</h1>')
+	}
+	function cambiarFondo2(){
+		$('#slider').css('background-image','url(img/portada1.png)')
+		$('#slider h1').html('<h1>Responsabilidad, y trabajo en equipo</h1>')
+	}
+	var nuevoFondo;
+	function myFunction(){
+		nuevoFondo = setInterval(cambiarFondo,10000);
+		nuevoFondo = setInterval(cambiarFondo1,20000);
+		nuevoFondo = setInterval(cambiarFondo2,30000);
+	}
+});
