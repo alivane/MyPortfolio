18d4221c4bd574799974766521802708a821d3da
AliVane <vaneg643@gmail.com>
2016-08-03 11:14:54 -0400

    insert of class to section-5 and footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-	var contador = 1 ;
 	$('.hola').css('display','none')
 	$('nav').css('display','none')
-	$("i").click(function(){
+	$(".bt-menu i").click(function(){
 		$('nav').toggle();
 		$('.hola').toggle();
 		$('.fa-bars').toggle();
