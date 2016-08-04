596a50c0e8c8e76475dc1f20978062031d402810
AliVane <vaneg643@gmail.com>
2016-08-04 16:16:04 -0400

    delete of class and insert of background to footer

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@ $(document).ready(function(){
 $(document).ready(function(){
 	myFunction()
 	function cambiarLetra(){
-		$('.contenido h1').html('<h1>Soy Front-End</h1>')
+		$('.contenido h1').html('<h1>soy Desarrolladora Front-End.</h1>')
 	}
 	function cambiarLetra1(){
-		$('.contenido h1').html('<h1>Tengo habilidades para Back-End</h1>')
+		$('.contenido h1').html('<h1>También tengo habilidades para Back-End, </h1>')
 	}
 	function cambiarLetra2(){
-		$('.contenido h1').html('<h1>Mi Lema Responsabilidad, y trabajo en equipo</h1>')
+		$('.contenido h1').html('<h1>mi lema responsabilidad y trabajo en equipo</h1>')
 	}
 	var nuevaLetra;
 	function myFunction(){
-		nuevaLetra = setInterval(cambiarLetra,5000);
-		nuevaLetra = setInterval(cambiarLetra1,10000);
-		nuevaLetra = setInterval(cambiarLetra2,15000);
+		nuevaLetra = setInterval(cambiarLetra,3000);
+		nuevaLetra = setInterval(cambiarLetra1,60000);
+		nuevaLetra = setInterval(cambiarLetra2,9000);
 	}
 });
