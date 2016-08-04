100b83cc72bb01ba9821d26bc21a1e6daab25b0a
AliVane <vaneg643@gmail.com>
2016-08-04 19:39:31 -0400

    insert of index for traductor

---
+++ js/main1.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('.hola').css('display','none')
+	$('nav').css('display','none')
+	$(".bt-menu i").click(function(){
+		$('nav').toggle();
+		$('.hola').toggle();
+		$('.fa-bars').toggle();
+	})
+})
+$(document).ready(function(){
+	myFunction()
+	function cambiarLetra(){
+		$('.contenido h1').html('<h1>I am a Front-End Developer.</h1>')
+	}
+	function cambiarLetra1(){
+		$('.contenido h1').html('<h1>También tengo habilidades para Back-End, </h1>')
+	}
+	function cambiarLetra2(){
+		$('.contenido h1').html('<h1>mi lema responsabilidad y trabajo en equipo</h1>')
+	}
+	var nuevaLetra;
+	function myFunction(){
+		nuevaLetra = setInterval(cambiarLetra,3000);
+		nuevaLetra = setInterval(cambiarLetra1,60000);
+		nuevaLetra = setInterval(cambiarLetra2,9000);
+
+	}
+});
+$(document).ready(function(){
+	$('.1').click(function(){
+		$('.jv').add('.css').add('.equipo').show();
+	})
+	$('.2').click(function(){
+		$('.jv').hide();
+		$('.css').show();
+	})
+	$('.3').click(function(){
+		$('.css').add('.equipo').hide();
+		$('.jv').show();
+	})
+	$('.4').click(function(){
+		$('.jv').add('.css').hide();
+		$('.equipo').show();
+	})
+})
